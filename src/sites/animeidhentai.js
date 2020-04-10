2e656dbd43568f2b7fb4a4a3e127eee83a92cb68
ngomile <ngomile932@gmail.com>
2020-04-10 10:36:58 +0200

    Added episode extraction for animeidhentai

--- src/sites/animeidhentai.js
+++ src/sites/animeidhentai.js
@@ -4,7 +4,9 @@ const cloudscraper = require('cloudscraper');
 const cheerio = require('cheerio');
 
 const searchURL = 'https://animeidhentai.com/search/';
+const apiURL = 'https://animeidhentai.com/wp-admin/admin-ajax.php';
 const urlReg = /<a href="([^"]+)/;
+const dataIDReg = /player" data-id="([^"]+)/;
 
 function getHeaders() {
     return {
@@ -50,7 +52,48 @@ async function getAnime(url) {
     return episodes.reverse();
 }
 
+async function getEpisode(title, url) {
+    let qualities, subTypes = new Map();
+    const page = await cloudscraper.get(url, { headers: getHeaders() });
+    const [, id] = page.match(dataIDReg);
+    const idToStream = new Map([['0', 'sub'], ['1', 'sub-2'], ['2', 'nosub']]);
+    const $ = cheerio.load(page);
+
+    $('.opt-player button').each(function (ind, element) {
+        const streamID = $(this).attr('data-opt');
+        const subType = idToStream.get(streamID);
+        subTypes.set(subType, streamID);
+    });
+
+    // TODO: This should be set by user
+    const subType = subTypes.get('sub-2') || subTypes.get('sub');
+    const formData = { id: id, action: 'ajax_player', opt: subType };
+    const streamPage = await cloudscraper.post(apiURL, {
+        headers: { ...getHeaders(), 'Referer': url },
+        formData: formData
+    });
+    let [, streamURL] = streamPage.match(/src="([^"]+)/);
+    streamURL = streamURL.replace('embed', 'download');
+    qualities = await extractKsplayer(streamURL);
+    return { title, qualities };
+}
+
+async function extractKsplayer(url) {
+    let qualities = new Map();
+    const page = await cloudscraper.get(url, { headers: getHeaders() });
+    const $ = cheerio.load(page);
+
+    $('.download_links a').each(function (ind, element) {
+        const quality = $(this).find('span').text().toLowerCase();
+        const source = $(this).attr('href');
+        qualities.set(quality, source);
+    });
+
+    return qualities;
+}
+
 module.exports = {
     search,
     getAnime,
+    getEpisode
 }
