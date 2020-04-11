df860bb720a279e35e948dc16a4ce23a02553988
ngomile <ngomile932@gmail.com>
2020-04-11 20:33:55 +0200

    Implemented stream url extraction for mp4upload

---
+++ src/extractors/mp4upload.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { exec } = require('child_process');
+
+const cloudscraper = require('cloudscraper');
+const beautify = require('js-beautify').js;
+
+const {
+    getHeaders
+} = require('../utils');
+
+exports.extract = async function ({ url, referer = '' }) {
+    referer = referer || url;
+    const page = await cloudscraper.get(url, { headers: getHeaders({ 'Referer': referer }) });
+    const [, obsfucatedJS] = page.match(/eval\((.*)\)/);
+    const deobsfucatedJS = eval(`const extract = () => (${obsfucatedJS}); extract()`);
+    const [, streamURL] = deobsfucatedJS.match(/player\.src\("([^"]+)/);
+    return { url: streamURL, referer: url };
+}
