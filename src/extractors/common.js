0c5a756f6edae3e8a2912c3440b5ee6bad9ab7d9
ngomile <ngomile932@gmail.com>
2020-05-01 00:22:28 +0200

    Added ExtractedInfo type for extractors

---
+++ src/extractors/common.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** 
+ * Represents the actual extracted url with the referer,
+ * must be created by the extractors extract function
+ * */
+class ExtractedInfo {
+    /**
+     * @param {string} url 
+     * @param {string} referer 
+     */
+    constructor(url, referer) {
+        this.url = url;
+        this.referer = referer;
+    }
+}
+
+module.exports = {
+    ExtractedInfo
+}
