84bcf450343fd28c93f9311b141a3bb5d24e9bef
ngomile <ngomile932@gmail.com>
2020-05-17 11:23:29 +0200

    Add yourupload extractor

---
+++ src/extractors/yourupload.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const cloudscraper = require('cloudscraper');
+
+const { ExtractedInfo } = require('./common');
+const { getHeaders } = require('../utils');
+
+/** Regular expression to match the source url of the video */
+const SOURCE_REG = /source src="([^"]+)/;
+
+/**
+ * Extracts stream url and referer from yourupload
+ *
+ * @param {object} obj
+ * @param {string} obj.url
+ * @param {string} [obj.referer]
+ * @returns {Promise<ExtractedInfo>} The extracted information
+ */
+module.exports.extract = async function ({ url, referer = '' }) {
+    referer = referer || url;
+    const page = await cloudscraper.get(url, { headers: getHeaders({ Referer: referer }) });
+    const [, streamURL] = page.match(/file:\s*'([^']+)/);
+    return new ExtractedInfo(streamURL, url);
+}
