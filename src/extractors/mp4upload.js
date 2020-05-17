1d3a652991346158b9ded6f956b96568f26b10ab
ngomile <ngomile932@gmail.com>
2020-05-17 11:27:43 +0200

    Improve regular expression for stream url

--- src/extractors/mp4upload.js
+++ src/extractors/mp4upload.js
@@ -15,19 +15,20 @@ const {
  * @param {string} [obj.referer]
  * @returns {Promise<ExtractedInfo>} The extracted information
  */
-exports.extract = async function ({ url, referer = '' }) {
+module.exports.extract = async function ({ url, referer = '' }) {
     referer = referer || url;
     let streamURL;
     const page = await cloudscraper.get(url, { headers: getHeaders({ Referer: referer }) });
     const deletedText = 'File was deleted';
     const licensedText = 'This video is no longer available due to a copyright claim.';
+
     if (page.includes(deletedText) || page.includes(licensedText)) {
         console.log(`The file has been deleted or licensed at ${url}`);
         streamURL = '';
     } else {
         const [, obsfucatedJS] = page.match(/eval\((.*)\)/);
         const deobsfucatedJS = eval(`const extract = () => (${obsfucatedJS}); extract()`);
-        [, streamURL] = deobsfucatedJS.match(/player\.src\("([^"]+)/);
+        [, streamURL] = deobsfucatedJS.match(/src[:\(]\s*"([^"]+)/);
     }
     return new ExtractedInfo(streamURL, url);
 }
