a8d1b3c44aa8d17810c2c18bdff259684f6837e2
ngomile <ngomile932@gmail.com>
2020-06-10 20:52:46 +0200

    Fix kwik extractor

--- src/extractors/kwik.js
+++ src/extractors/kwik.js
@@ -1,11 +1,13 @@
 const request = require('../request');
 const { ExtractedInfo } = require('./common');
 const {
+    executeCommand,
     getHeaders,
     bypassCaptcha,
     generateFormData
 } = require('../utils');
 
+const DEFAULT_HEADERS = getHeaders();
 const OPTIONS = {
     simple: false,
     resolveWithFullResponse: true,
@@ -19,12 +21,11 @@ let passToken;
  *
  * @param {object} obj
  * @param {string} obj.url
- * @param {string} [obj.referer]
  * @returns {Promise<ExtractedInfo>} The extracted information
  */
 module.exports.extract = async function ({ url }) {
     url = url.replace(/\be\b/, 'f');
-    const headers = getHeaders({ Referer: url });
+    const headers = getHeaders({ ...DEFAULT_HEADERS, Referer: url });
     let response;
     if (!passToken) {
         passToken = await bypassCaptcha(url, headers);
@@ -34,11 +35,12 @@ module.exports.extract = async function ({ url }) {
             form,
             ...OPTIONS
         });
+    } else {
+        response = await request.get(url, { headers, ...OPTIONS });
     }
 
-    const [, obsfucatedJS] = response.body.match(/(var _.*?=.*)/);
-    let deobsfucatedJS;
-    eval(`eval = input => {deobsfucatedJS = input;}; ${obsfucatedJS}`);
+    const [, obsfucatedJS] = response.body.match(/(var _[\w]+=.*)/);
+    const deobsfucatedJS = await executeCommand('node', ['-e', `eval=console.log; ${obsfucatedJS}`]);
     const [, postURL, _token] = deobsfucatedJS.match(/action="([^"]+).*value="([^"]+)/);
     const form = { _token };
     response = await request.post(postURL, { headers, form, ...OPTIONS });
