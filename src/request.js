aa35ce7ef65be72ab2ca9d8ed70a8f36e02c62c3
ngomile <ngomile932@gmail.com>
2022-07-04 22:54:06 +0200

    Simplifies request handling logic and makes request take cf argument as parameter to options instead of argument

--- src/request.js
+++ src/request.js
@@ -4,57 +4,61 @@ const path = require('path');
 const fs = require('fs');
 
 const FileCookieStore = require('tough-cookie-filestore');
-let rp = require('request-promise');
-let cloudscraper = require('cloudscraper');
 
 const { CONFIG_DIR } = require('./config');
-
 const COOKIES_FILE = path.join(CONFIG_DIR, 'cookies.json');
+
 if (!fs.existsSync(COOKIES_FILE)) {
     fs.mkdirSync(CONFIG_DIR, { recursive: true });
     fs.writeFileSync(COOKIES_FILE, '');
 }
 
-const jar = rp.jar(new FileCookieStore(COOKIES_FILE));
-rp = rp.defaults({ jar, timeout: 30000, json: true });
-cloudscraper = cloudscraper.defaults({ jar: true, timeout: 30000, json: true });
+const requestOptions = { timeout: 30000, json: true };
+
+const rp = require('request-promise').defaults({
+    jar: new FileCookieStore(COOKIES_FILE),
+    ...requestOptions,
+});
+
+const cloudscraper = require('cloudscraper').defaults({
+    jar: true,
+    ...requestOptions,
+});
 
 const delay = numRetry =>
     Math.pow(2, numRetry) * 1000 + Math.floor(Math.random() * 1000);
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-async function request(url, options, cf = false) {
+async function request(url, { cf, ...options }) {
     const requestHandler = cf ? cloudscraper : rp;
-    let retries = 0;
-    const httpRequest = async (url, options) => {
-        try {
-            return await requestHandler(url, options);
-        } catch (error) {
-            retries++;
-            if (retries < 10) {
-                await timeout(delay(retries));
-                return await httpRequest(url, options);
-            }
-            throw error;
-        }
-    };
 
-    try {
-        return await httpRequest(url, options);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
+    let retries = 0;
+    while (true) {
+        try {
+            const result = await requestHandler(url, options);
+            return result;
+        } catch (error) {
+            if (retries < 10) {
+                retries++;
+                await timeout(delay(retries));
+                continue;
+            }
+
+            throw new Error('Request Error', { cause: error });
+        }
     }
 }
 
-async function get(url, options = {}, cf = false) {
+async function get(url, options = {}) {
     options.method = 'GET';
-    return await request(url, options, cf);
+    const result = await request(url, options);
+    return result;
 }
 
-async function post(url, options = {}, cf = false) {
+async function post(url, options = {}) {
     options.method = 'POST';
-    return await request(url, options, cf);
+    const result = await request(url, options);
+    return result;
 }
 
 module.exports = {
